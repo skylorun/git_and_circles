04226792ca43e66c865bdfbbeb0762c7c7351945
skylorun <tolyarush@gmail.com>
2021-02-04 16:57:47 +0700

    second try

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import sys
+from random import randrange
 
 from PIL import Image, ImageDraw
-from PyQt5 import uic
-from PyQt5.QtGui import QPixmap, QPainter, QColor
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from random import randrange
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QWidget
 
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('UI.ui', self)
-        self.but.clicked.connect(self.a)
+        self.b = A()
+        self.b.show()
+
+
+class A(QWidget):
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle('Кружочки')
+        self.resize(500, 500)
+        self.square = QLabel(self)
+        self.square.resize(581, 421)
+        self.square.move(0, 0)
+        self.but = QPushButton(self)
+        self.but.resize(131, 81)
+        self.but.move(210, 150)
+        self.but.setText('Показать круги')
+        self.but.clicked.connect(self.a)
 
     def a(self):
         self.but.hide()
@@ -25,8 +38,10 @@ class Window(QMainWindow):
         b = randrange(300, 470, 15)
         c = randrange(50, 300, 10)
         d = randrange(300, 470, 15)
-        dr.ellipse((a, a, b, b), outline="#FFFF00", width=10)
-        dr.ellipse((c, c, d, d), outline="#FFFF00", width=10)
+        dr.ellipse((a, a, b, b), outline=(randrange(0, 256), randrange(0, 256), randrange(0, 256)),
+                   width=10)
+        dr.ellipse((c, c, d, d), outline=(randrange(0, 256), randrange(0, 256), randrange(0, 256)),
+                   width=10)
         im.save('res.jpg')
         z = QPixmap('res.jpg')
         self.square.setPixmap(z)
@@ -34,6 +49,6 @@ class Window(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    wnd = Window()
+    wnd = A()
     wnd.show()
     sys.exit(app.exec())
